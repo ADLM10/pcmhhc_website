e636b6707b58206cf04883cc82646554529a4137
Alimpan De <de.alimpan5@gmail.com>
2023-01-24 20:03:17 +0530

    updated:notice board and responsiveness

--- src/firebase.js
+++ src/firebase.js
@@ -2,23 +2,25 @@ import { getStorage } from "@firebase/storage";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAvTN1GzYHYRXN6_USUrlmS3gPTr_cO7So",
-  authDomain: "pchhmc-website.firebaseapp.com",
-  projectId: "pchhmc-website",
-  storageBucket: "pchhmc-website.appspot.com",
-  messagingSenderId: "764101601877",
-  appId: "1:764101601877:web:e2a9577f4b788694ec42dc",
-  measurementId: "G-XG3B8TC75R",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+export const db = getFirestore(app);
 export const storage = getStorage(app);
